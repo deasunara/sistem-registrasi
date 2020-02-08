193392b39c0ef38d71ce92027b9203b2acabdc0e
Dea Sunara <deasunara@gmail.com>
2020-02-08 14:07:49 +0700

    updates koneksi

--- koneksi.php
+++ koneksi.php
@@ -1,4 +1,11 @@
 <?php
-$con = mysqli_connect("localhost","root","","regis_seminar");
-// $db = mysql_select_db("db_stok");
+$servername = "localhost";
+$username = "root";
+$password = "";
+$db = "regis_seminar";
+
+$con = mysqli_connect($servername,$username,$password,$db);
+if ($con->connect_error) {
+    die("Connection failed: " . $con->connect_error);
+    }
 ?>
